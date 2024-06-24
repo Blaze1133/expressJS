72fed5614d93fae894d5b4624b5ee7c6c112d083
Blaze <manikanta3.mk1@gmail.com>
2024-06-25 00:36:57 +0530

    small modifications

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const http = require('http');
 
 const server = http.createServer((req,res) =>{
-    res.writeHead(200,'content-type: text/html')
-    res.write('<h1>HELLO WORLD</h1>')
-    res.end();
-
+    const url = req.url
+    if(url === '/'){
+        console.log(req.method)
+        res.writeHead(200,{'Content-type': 'text/html'})
+        res.write('<h1>HOME PAGE</h1>')
+        res.end();
+    }
+    else if(url === '/about'){
+        res.writeHead(200,{'Content-type': 'text/html'})
+        res.write('<h1>ABOUT PAGE</h1>')
+        res.end();
+    }
+    else{
+        res.writeHead(400,{'Content-type': 'text/html'})
+        res.write('<h1>PAGE NOT FOUND </h1>')
+        res.end();
+    }
 })
-s
 server.listen(5000);
