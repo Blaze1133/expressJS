c4973a66f9a950ba3a2e31bd050bc0d33c34243a
Blaze <manikanta3.mk1@gmail.com>
2024-06-25 20:47:48 +0530

    creating server with http module

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const http = require('http');
+const {readFileSync} = require('fs');
+
+const homePage = readFileSync('./Calculator/index.html'); 
+const CSSPage = readFileSync('./Calculator/style.css'); 
+const JSPage = readFileSync('./Calculator/script.js'); 
+
 
 const server = http.createServer((req,res) =>{
     const url = req.url
+    console.log(url)
     if(url === '/'){
         console.log(req.method)
         res.writeHead(200,{'Content-type': 'text/html'})
-        res.write('<h1>HOME PAGE</h1>')
+        res.write(homePage)
+        res.end();
+    }
+    else if(url === '/style.css'){
+        res.writeHead(200,{'Content-type': 'text/css'})
+        res.write(CSSPage)
+        res.end();
+    }
+    else if(url === '/script.js'){
+        res.writeHead(200,{'Content-type': 'text/javascript'})
+        res.write(JSPage)
         res.end();
     }
     else if(url === '/about'){
